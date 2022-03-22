c93b4f3cc967985536bf2f513d7d69c056841b5e
Jenci <jenci2114@gmail.com>
2022-03-22 17:38:43 -0400

    Completed milestone 1a

--- frogger.asm
+++ frogger.asm
@@ -19,3 +19,82 @@
 # - None
 #
 ###########################################################
+
+.data
+displayAddress: 	.word 0x10008000
+
+frogColour: 		.word 0x69db41
+
+safeRegionColour: 	.word 0x8b1ad6
+goalRegionColour: 	.word 0x4ea333
+roadColour: 		.word 0x808080
+waterColour:		.word 0x000044
+
+logColour: 		.word 0xcf6f50
+vehicleColour: 		.word 0x000000
+	
+.text
+
+lw $t0, displayAddress 			# $t0 = displayAddress;
+li $t1, 0 				# $t1 = 0;
+lw $t2, goalRegionColour		# $t2 = goalRegionColour;
+
+DrawGoalRegion:
+beq $t1, 512, DrawGoalRegionEnd 	# while ($t1 != 512) {
+add $t3, $t0, $t1			#	$t3 = $t0 + $t1	
+sw $t2, 0($t3)				# 	*($t3) = $t2;
+addi $t1, $t1, 4			# 	$t1 += 4;
+j DrawGoalRegion			# }
+
+DrawGoalRegionEnd:			# $t1 == 512 at this point
+
+lw $t2, waterColour			# $t2 = waterColour
+				
+DrawWater:
+beq $t1, 2048, DrawWaterEnd 		# while ($t1 != 2048) {
+add $t3, $t0, $t1			#	$t3 = $t0 + $t1	
+sw $t2, 0($t3)				# 	*($t3) = $t2;
+addi $t1, $t1, 4			# 	$t1 += 4;
+j DrawWater				# }
+
+DrawWaterEnd:				# $t1 == 2048 at this point
+
+lw $t2, safeRegionColour 		# $t2 = safeRegionColour
+
+DrawSafeRegion:
+beq $t1, 2560, DrawSafeRegionEnd 	# while ($t1 != 2560) {
+add $t3, $t0, $t1			#	$t3 = $t0 + $t1	
+sw $t2, 0($t3)				# 	*($t3) = $t2;
+addi $t1, $t1, 4			# 	$t1 += 4;
+j DrawSafeRegion			# }
+
+DrawSafeRegionEnd:			# $t1 == 2560 at this point
+
+lw $t2, roadColour 			# $t2 = roadColour
+
+DrawRoad:
+beq $t1, 3584, DrawRoadEnd 		# while ($t1 != 3584) {
+add $t3, $t0, $t1			#	$t3 = $t0 + $t1	
+sw $t2, 0($t3)				# 	*($t3) = $t2;
+addi $t1, $t1, 4			# 	$t1 += 4;
+j DrawRoad				# }
+
+DrawRoadEnd:				# $t1 = 3584 at this point
+
+lw $t2, safeRegionColour 		# $t2 = safeRegionColour
+
+DrawStartRegion:
+beq $t1, 4096, DrawStartRegionEnd 	# while ($t1 != 4096) {
+add $t3, $t0, $t1			#	$t3 = $t0 + $t1	
+sw $t2, 0($t3)				# 	*($t3) = $t2;
+addi $t1, $t1, 4			# 	$t1 += 4;
+j DrawStartRegion				# }
+
+DrawStartRegionEnd:			# $t1 = 3584 at this point
+
+Exit:
+li $v0, 10
+syscall
+
+
+
