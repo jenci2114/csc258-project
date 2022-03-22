379429a15933ee07b9940d5f183680c90c20128d
Jenci <jenci2114@gmail.com>
2022-03-22 16:30:24 -0400

    Added preamble

---
+++ frogger.asm
@@ -0,0 +1,21 @@
+###########################################################
+#
+# CSC258H1S Winter 2022 Assembly Final Project
+# University of Toronto, St. George
+#
+# Name: Jenci Wei
+# Student Number: 1006670498
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 8
+# - Unit height in pixels: 8
+# - Display width in pixels: 256
+# - Display height in pixels: 256
+# - Base address for display: 0x10008000 ($gp)
+#
+# Milestone reached: 0
+# 
+# Additional features implemented:
+# - None
+#
+###########################################################
